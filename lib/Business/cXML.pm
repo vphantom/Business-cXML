0cdae1b9dc521918ecb5620784c82b1a73dc5f97
Stéphane Lavergne <lis@imars.com>
2018-02-01 10:45:05 -0500

    Release v0.6.4

--- lib/Business/cXML.pm
+++ lib/Business/cXML.pm
@@ -108,7 +108,7 @@ use Scalar::Util 'blessed';
 use Business::cXML::Transmission;
 
 BEGIN {
-	our $VERSION = 'v0.6.3';
+	our $VERSION = 'v0.6.4';
 	our $CXML_VERSION = '1.2.036';
 	our $USERAGENT = "Business::cXML.pm $VERSION";
 }
@@ -535,6 +535,10 @@ sub stringify {
 }
 
 =back
+
+=head1 VERSION
+
+0.6.4 based on cXML DTD 1.2.036
 
 =head1 AUTHOR
 
